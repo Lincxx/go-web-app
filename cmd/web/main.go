7c1130f3048f8ed97de76baa9eb9e8b19fc99caa
lincxx <bossc@BOSSCO>
2023-12-26 16:07:20 -0500

    Setting application wide configuration

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/Lincxx/go-web-app/pkg/config"
 	"github.com/Lincxx/go-web-app/pkg/handlers"
+	"github.com/Lincxx/go-web-app/pkg/render"
+	"log"
 	"net/http"
 )
 
@@ -10,6 +13,14 @@ const portNumber = ":8080"
 
 // main is the main application function
 func main() {
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
 
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,1 +1,8 @@
 package config
+
+import "html/template"
+
+// Appconfig holds the application config
+type AppConfig struct {
+	TemplateCache map[string]*template.Template
+}
