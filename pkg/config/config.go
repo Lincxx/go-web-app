1efbcdab7493d3863302f672015170867f851f5e
lincxx <bossc@BOSSCO>
2023-12-26 16:08:39 -0500

    Why the application wide config is so useful

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,8 +1,12 @@
 package config
 
-import "html/template"
+import (
+	"html/template"
+	"log"
+)
 
 // Appconfig holds the application config
 type AppConfig struct {
 	TemplateCache map[string]*template.Template
+	InfoLog       *log.Logger
 }
